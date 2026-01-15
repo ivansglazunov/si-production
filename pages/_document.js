d4dc306f86b2f7d46c820102ebd59454577bbe65
ae <ae.registrator@atomicmail.io>
2026-01-15 18:40:53 +0300

    Refactor Yandex.Metrika integration in Document component to use Next.js Script component for improved loading strategy. Update inline styles for noscript image to use JSX syntax. Maintain HTML language setting as Russian.

--- pages/_document.js
+++ pages/_document.js
@@ -1,32 +1,33 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="ru">
       <Head>
         {/* Yandex.Metrika counter */}
-        <script
-          type="text/javascript"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(m,e,t,r,i,k,a){
-                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-                m[i].l=1*new Date();
-                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-              })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=106277268', 'ym');
+        <Script
+          id="yandex-metrika"
+          strategy="beforeInteractive"
+        >
+          {`
+            (function(m,e,t,r,i,k,a){
+              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=106277268', 'ym');
 
-              ym(106277268, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
-            `,
-          }}
-        />
+            ym(106277268, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+          `}
+        </Script>
       </Head>
       <body>
         <Main />
         <NextScript />
         <noscript>
           <div>
-            <img src="https://mc.yandex.ru/watch/106277268" style="position:absolute; left:-9999px;" alt="" />
+            <img src="https://mc.yandex.ru/watch/106277268" style={{position: 'absolute', left: '-9999px'}} alt="" />
           </div>
         </noscript>
       </body>
