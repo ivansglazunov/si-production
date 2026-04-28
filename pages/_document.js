a7323ac25b72fee30476cfefcfcfc14e628cd04f
Ivan S Glazunov <ivansglazunov@gmail.com>
2026-04-29 00:53:17 +0300

    Fix Yandex.Metrika: restore working init code, move noscript to head

--- pages/_document.js
+++ pages/_document.js
@@ -14,27 +14,18 @@ export default function Document() {
         <Script
           id="yandex-metrika"
           strategy="beforeInteractive"
-          src="https://mc.yandex.ru/metrika/tag.js?id=106277268"
-        />
-        <Script
-          id="yandex-metrika-init"
-          strategy="beforeInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-              m[i].l=1*new Date();for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        >
+          {`
+            (function(m,e,t,r,i,k,a){
+              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
               k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-              )(
-              window,
-              document,
-              'script',
-              'https://mc.yandex.ru/metrika/tag.js?id=106277268',
-              'ym'
-              );
-              ym(106277268, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
-            `,
-          }}
-        />
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=106277268', 'ym');
+
+            ym(106277268, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+          `}
+        </Script>
       </Head>
       <body>
         <Main />
