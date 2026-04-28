afbb3bd02aea8d095a3f58faa5f54752b233d5dd
Ivan S Glazunov <ivansglazunov@gmail.com>
2026-04-29 00:39:12 +0300

    Add Yandex.Metrika tracking with noscript fallback

--- pages/_document.js
+++ pages/_document.js
@@ -6,30 +6,39 @@ export default function Document() {
     <Html lang="ru">
       <Head>
         {/* Yandex.Metrika counter */}
-        <Script
-          id="yandex-metrika"
-          strategy="beforeInteractive"
-        >
-          {`
-            (function(m,e,t,r,i,k,a){
-              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-              m[i].l=1*new Date();
-              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=106277268', 'ym');
-
-            ym(106277268, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
-          `}
-        </Script>
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
         <noscript>
           <div>
             <img src="https://mc.yandex.ru/watch/106277268" style={{position: 'absolute', left: '-9999px'}} alt="" />
           </div>
         </noscript>
+        <Script
+          id="yandex-metrika"
+          strategy="beforeInteractive"
+          src="https://mc.yandex.ru/metrika/tag.js?id=106277268"
+        />
+        <Script
+          id="yandex-metrika-init"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              )(
+              window,
+              document,
+              'script',
+              'https://mc.yandex.ru/metrika/tag.js?id=106277268',
+              'ym'
+              );
+              ym(106277268, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+            `,
+          }}
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
       </body>
     </Html>
   )
